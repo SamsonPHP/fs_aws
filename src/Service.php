2e128ed0648f745e4e79fb6515d8120f5a116326
Vitaly Egorov <egorov@samsonos.com>
2015-01-22 15:56:39 +0200

    Added service class for loading into SamsonPHP projects

---
+++ src/Service.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: egorov
+ * Date: 22.01.2015
+ * Time: 15:55
+ */
+namespace samsonphp\fs\aws;
+
+/**
+ * SamsonPHP AWS Service class
+ * @package samsonphp\fs\aws
+ */
+class Service extends \samson\core\CompressableService
+{
+    /** @var string Module identifier */
+    public $id = 'fs_aws';
+}
